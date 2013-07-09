30c5711690e4b84e7511f0abee0aefb3a25753de
Christopher Harris <chris_jh@me.com>
2013-07-09 22:20:50 +0100

    1. added start of EmuScanner 1. added start of EmuConfig

---
+++ src/main/java/com/tgm/emu/EmuScanner.java
@@ -0,0 +1,38 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tgm.emu;
+
+import java.util.Map;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+
+/**
+ *
+ * @author christopher
+ */
+public class EmuScanner implements InitializingBean {
+
+    @Autowired
+    private ApplicationContext context;
+    
+    public void afterPropertiesSet() throws Exception {
+        Logger.getLogger(this.getClass()).info("EmuScanner Initilize");
+        
+        for (Map.Entry<String, EmuConfig> entry : context.getBeansOfType(EmuConfig.class).entrySet()) {
+            Logger.getLogger(this.getClass()).info("Loading Emu Config: "+entry.getValue().getName());
+            
+        }
+    }
+
+    /**
+     * @param context the context to set
+     */
+    public void setContext(ApplicationContext context) {
+        this.context = context;
+    }
+    
+}
