33a25990579a6500e3443f36d86d7f67fb4c5760
Christopher Harris <chris_jh@me.com>
2013-07-10 21:52:53 +0100

    1. added loading of property files 2. moved config files around so not in jar file for external editing 3. added resolution config option into netbeans for debugging

---
+++ src/main/java/com/tgm/resources/TgmResource.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tgm.resources;
+
+/**
+ *
+ * @author christopher
+ */
+public class TgmResource {
+    
+    public final static String MEDIA = "media";
+    
+}
