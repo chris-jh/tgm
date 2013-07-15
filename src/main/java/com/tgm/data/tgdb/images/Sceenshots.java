26f54db984599312bbc44e7df6c1acdb8631f320
Mark Riley <markriley@gmail.com>
2013-07-15 21:34:04 +0100

    Added more stuff for the Games DB including:
     - Box Art
     - Fan Art (renamed from GameFanArt)
     - Game Ratings
     - Screenshots and
     - Trailers

---
+++ src/main/java/com/tgm/data/tgdb/images/Sceenshots.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tgm.data.tgdb.images;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author christopher
+ */
+@XmlRootElement(name = "fanart")
+public class Sceenshots {
+    
+    
+    
+}
